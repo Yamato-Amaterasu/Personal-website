5015b8927ab0c2de32401dc36975c13557d557f7
Mohamad Ayyad <hamoodaayyad@gmail.com>
2023-02-23 17:17:43 -0600

    fixing deploy

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,6 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "personal",
+  site: "https://my-site.com/",
   integrations: [mdx(), sitemap(), tailwind()],
 });
